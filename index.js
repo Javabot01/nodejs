5dd5e308f3ccefdd0a3663d23cecab3033ca564f
Javabot <davidoyebode01@gmail.com>
2020-04-30 14:38:31 +0100

    Basic workspace setup

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("This is my Index");
+//jshint esversion:8
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My App'
+    });
+});
+
+app.listen(2000, () => {
+    console.log('Server started on port 6000');
+
+});
+
+// const server = http.createServer((req, res) => {
+//     res.write('Hello World');
+//     res.end();
+// })
+
+// const port = 3000;
+
+// server.listen(port, () => {
+//     console.log("server started on port:", port);
+
+// })
